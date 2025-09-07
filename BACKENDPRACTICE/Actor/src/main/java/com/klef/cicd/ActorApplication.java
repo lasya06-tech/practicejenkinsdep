72d45630f80455a29560d4256e1714ea97693fcf
lasya06-tech <2300030060cseh@gmail.com>
2025-09-07 21:20:45 +0530

    Added extends servletinitializer

--- BACKENDPRACTICE/Actor/src/main/java/com/klef/cicd/ActorApplication.java
+++ BACKENDPRACTICE/Actor/src/main/java/com/klef/cicd/ActorApplication.java
@@ -2,12 +2,13 @@ package com.klef.cicd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ActorApplication {
+public class ActorApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ActorApplication.class, args);
-		System.out.println("Project is running");
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ActorApplication.class, args);
+        System.out.println("Project is running");
+    }
 }
